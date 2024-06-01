542988711ab86a16e43bb1224f517c0024db2ede
Ajithkumar-ai <108.ajith5@gmail.com>
2024-06-01 12:42:37 +0530

    adding mutiplication and division functionalities

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
   const first= useRef(null);
   const second = useRef(null);
   const operator = useRef(null);
+  const operators = ['+',"-","*","/"];
   const performCalculation = (first,second,operator)=>{
     if(first.current && second.current && operator.current) {
       let res=0;
       if(operator.current==='+'){res =first.current+second.current}
       if(operator.current==='-'){res =first.current-second.current}
+      if(operator.current==='*'){res =first.current*second.current}
+      if(operator.current==='/'){res =first.current/second.current}
       first.current=res;
       second.current=null;
       operator.current=null;
@@ -21,7 +24,13 @@ function App() {
     }
   };
   const onClickHandler=(number)=>{
-    if((number==='+'||number==='-') && first.current) {
+    if(number==='AC') {
+      first.current=null;
+      second.current=null;
+      operator.current=null;
+      setValue(0);
+    }
+    else if((operators.includes(number)) && first.current) {
       performCalculation(first,second,operator);
       let displayValue = '';
       if(first.current && second.current){displayValue=(first.current + number+ second.current).toString();}
@@ -32,7 +41,7 @@ function App() {
     else if(number === '=') {
       performCalculation(first,second,operator);
     }
-    else if(number!=='+'&& number!=='-'){
+    else if(!operators.includes(number)){
         if(!first.current) {
           first.current=parseInt(number.toString());
           setValue(first.current);
@@ -66,18 +75,24 @@ function App() {
             <Button onClick={onClickHandler} number={7}></Button>
             <Button onClick={onClickHandler} number={8}></Button>
             <Button onClick={onClickHandler} number={9}></Button>
-            <Button onClick={onClickHandler} number={'+'}></Button>
+            <Button onClick={onClickHandler} number={'AC'}></Button>
           </div>
           <div className='buttonflex'>
             <Button onClick={onClickHandler} number={6}></Button>
             <Button onClick={onClickHandler} number={5}></Button>
             <Button onClick={onClickHandler} number={4}></Button>
-            <Button onClick={onClickHandler} number={'-'}></Button>
+            <Button onClick={onClickHandler} number={'+'}></Button>
           </div>
           <div className='buttonflex'>
             <Button onClick={onClickHandler} number={3}></Button>
             <Button onClick={onClickHandler} number={2}></Button>
             <Button onClick={onClickHandler} number={1}></Button>
+            <Button onClick={onClickHandler} number={'-'}></Button>
+          </div>
+          <div className='buttonflex'>
+            <Button onClick={onClickHandler} number={0}></Button>
+            <Button onClick={onClickHandler} number={'*'}></Button>
+            <Button onClick={onClickHandler} number={'/'}></Button>
             <Button onClick={onClickHandler} number={'='}></Button>
           </div>
         </div>
